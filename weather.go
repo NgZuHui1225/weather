e904435ebcf65639bc51def738969d57273ac0e2
NgZuHui1225 <154544549+NgZuHui1225@users.noreply.github.com>
2024-06-06 14:04:58 +0800

    Add files via upload
    +post & get

--- weather.go
+++ weather.go
@@ -4,57 +4,108 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
+	"net/url"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/go-resty/resty/v2"
 )
 
-const apiKey = "FZ4UNKA5K4BXQUBLYWT3NLT2A"
-const apiEndpoint = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/%s/%s/%s?unitGroup=metric&key=%s"
+const apiKey = "L4V2B56VD6YY8KCJCJBB6DUSK"
+const apiEndpoint = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"
 
 type WeatherResponse struct {
 	Days []struct {
 		Date          string  `json:"datetime"`
 		Temperature   float64 `json:"temp"`
 		Precipitation float64 `json:"precip"`
-		// Add more fields as needed
 	} `json:"days"`
 }
 
+type QueryParams struct {
+	Location  string `json:"location"`
+	StartDate string `json:"start_date"`
+	EndDate   string `json:"end_date"`
+}
+
+type ParamData struct {
+	data []QueryParams
+}
+
 func main() {
-	location := "Kuala Lumpur, KL"
-	startDate := "2024-06-01"
-	endDate := "2024-06-03"
+	r := chi.NewRouter()
+	store := &ParamData{
+		data: make([]QueryParams, 0),
+	}
 
 	client := resty.New()
 
-	url := fmt.Sprintf(apiEndpoint, location, startDate, endDate, apiKey)
+	//POST METHOD
+	r.Post("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("POST / endpoint hit")
+		var param QueryParams
+		if err := json.NewDecoder(r.Body).Decode(&param); err != nil {
+			log.Println("Error decoding JSON:", err)
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
 
-	resp, err := client.R().
-		//SetQueryParam("key", apiKey).
-		//SetQueryParam("location", location).
-		//SetQueryParam("startDate", startDate).
-		//SetQueryParam("endDate", endDate).
-		SetHeader("Content-Type", "application/json").
-		//Get(apiEndpoint)
-		Get(url)
+		log.Printf("Received parameters: %+v\n", param)
 
-	if err != nil {
-		log.Fatalf("Error making request: %v", err)
+		apiURL := fmt.Sprintf("%s%s/%s/%s", apiEndpoint, param.Location, param.StartDate, param.EndDate)
+		params := url.Values{}
+		params.Add("key", apiKey)
+		params.Add("unitGroup", "metric")
+
+		log.Printf("Requesting weather data from: %s\n", apiURL)
+
+		resp, err := client.R().
+			SetQueryParamsFromValues(params).
+			SetHeader("Content-Type", "application/json").
+			Get(apiURL)
+
+		if err != nil {
+			log.Println("Error making request: ", err)
+			http.Error(w, "Error making request to weather API", http.StatusInternalServerError)
+			return
+		}
+
+		if resp.StatusCode() != 200 {
+			log.Printf("Error: %v", resp)
+			http.Error(w, "Error from weather API", resp.StatusCode())
+			return
+		}
+
+		var weatherData WeatherResponse
+		err = json.Unmarshal(resp.Body(), &weatherData)
+		if err != nil {
+			log.Println("Error decoding JSON response: ", err)
+			http.Error(w, "Error decoding JSON response from weather API", http.StatusInternalServerError)
+			return
+		}
+
+		log.Printf("Weather data: %+v\n", weatherData)
+
+		store.data = append(store.data, param)
+
+		w.Header().Set("Content-Type", "application/json")
+		if err := json.NewEncoder(w).Encode(weatherData); err != nil {
+			log.Printf("Error encoding response: %v\n", err)
+		}
+	})
+
+	//GET METHOD
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("GET / endpoint hit")
+		data := store.data
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(data)
+	})
+
+	//localhost:3002
+	log.Println("Starting server on :3002")
+	if err := http.ListenAndServe(":3002", r); err != nil {
+		log.Fatal(err)
 	}
 
-	if resp.StatusCode() != 200 {
-		log.Fatalf("Error: %v", resp)
-	}
-
-	var weatherData WeatherResponse
-	err = json.Unmarshal(resp.Body(), &weatherData)
-	if err != nil {
-		log.Fatalf("Error decoding JSON response: %v", err)
-	}
-
-	// Print weather data
-	fmt.Printf("Weather for %s from %s to %s:\n", location, startDate, endDate)
-	for _, day := range weatherData.Days {
-		fmt.Printf("Date: %s, Temperature: %.2fC, Precipitation: %.2fmm\n", day.Date, day.Temperature, day.Precipitation)
-	}
 }
